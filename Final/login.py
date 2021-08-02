3745e84bba2c33d44cb9341c286b9ec84de2ea2b
Charllys Brauwol <charllysbrauwolhaha@gmail.com>
2021-08-02 04:56:00 -0300

    Testes

--- Final/login.py
+++ Final/login.py
@@ -19,85 +19,8 @@ def login():
     else :
         print("Erro")
 
-#Função do Botão de Listar Funcionários Cadastrados
-def listar_dados_funcionarios():
-    tela_funcionarios_2.show()
-    banco = sqlite3.connect('banco_funcionario.db') 
-    cursor = banco.cursor()
-    cursor.execute("SELECT * FROM dados")
-    dados_lidos = cursor.fetchall()
-    tela_funcionarios_2.tableWidget.setRowCount(len(dados_lidos))
-    tela_funcionarios_2.tableWidget.setColumnCount(5)
-
-    for i in range(0, len(dados_lidos)):
-        for j in range(0, 5):
-           tela_funcionarios_2.tableWidget.setItem(i,j,QtWidgets.QTableWidgetItem(str(dados_lidos[i][j])))
-    
-    banco.close()
-    
-#Função do Botão de Cadastrar Funcionarios
-def salvar_dados_funcionarios():
-    tela_funcionarios.show()
-    nome_completo = tela_funcionarios.lineEdit.text()
-    rg = tela_funcionarios.lineEdit_2.text()
-    cpf = tela_funcionarios.lineEdit_3.text()
-    telefone = tela_funcionarios.lineEdit_4.text()
-    endereco = tela_funcionarios.lineEdit_5.text()
-
-    try:
-        banco = sqlite3.connect('banco_funcionario.db') 
-        cursor = banco.cursor()
-        cursor.execute("CREATE TABLE IF NOT EXISTS dados (nome_completo text, rg text, cpf text, telefone text, endereco text)")
-        cursor.execute("INSERT INTO dados VALUES ('"+nome_completo+"','"+rg+"','"+cpf+"','"+telefone+"','"+endereco+"')")
-        banco.commit() 
-        banco.close()
-        tela_funcionarios.lineEdit.setText("")
-        tela_funcionarios.lineEdit_2.setText("")
-        tela_funcionarios.lineEdit_3.setText("")
-        tela_funcionarios.lineEdit_4.setText("")
-        tela_funcionarios.lineEdit_5.setText("")
-        print("Dados inseridos com sucesso!")
-
-    except sqlite3.Error as erro:
-        print("Erro ao inserir os dados: ",erro)
-
-#Função do Botão de Listar Cidades Cadastradas
-def listar_dados_cidades():
-    tela_cidades_2.show()
-    banco = sqlite3.connect('banco_cidades.db') 
-    cursor = banco.cursor()
-    cursor.execute("SELECT * FROM dados")
-    dados_lidos = cursor.fetchall()
-    tela_cidades_2.tableWidget.setRowCount(len(dados_lidos))
-    tela_cidades_2.tableWidget.setColumnCount(3)
-
-    for i in range(0, len(dados_lidos)):
-        for j in range(0, 3):
-           tela_cidades_2.tableWidget.setItem(i,j,QtWidgets.QTableWidgetItem(str(dados_lidos[i][j])))
-    
-    banco.close()
-    
-#Função do Botão de Cadastrar Cidades
-def salvar_dados_cidades():
-    tela_cidades.show()
-    cidade = tela_cidades.lineEdit.text()
-    endereco = tela_cidades.lineEdit_2.text()
-    ano = tela_cidades.lineEdit_3.text()
-    
-    try:
-        banco = sqlite3.connect('banco_cidades.db') 
-        cursor = banco.cursor()
-        cursor.execute("CREATE TABLE IF NOT EXISTS dados (cidade text,endereco text,ano text)")
-        cursor.execute("INSERT INTO dados VALUES ('"+cidade+"','"+endereco+"','"+ano+"')")
-        banco.commit() 
-        banco.close()
-        tela_cidades.lineEdit.setText("")
-        tela_cidades.lineEdit_2.setText("")
-        tela_cidades.lineEdit_3.setText("")
-        print("Dados inseridos com sucesso!")
-
-    except sqlite3.Error as erro:
-        print("Erro ao inserir os dados: ",erro)
+def tela_menu_principal():
+    tela_menu.show()
 
 #Função do Botão de Listar Veículos Cadastrados
 def listar_dados_veiculos():
@@ -170,7 +93,7 @@ def listar_dados_viagens():
 
 #Função Menu Principal Veículos
 def menu_veiculos():
-    tela_veiculos_menu_veiculos.show()
+    tela_menu_veiculos.show()
 
 def tela_cadastrar_veiculos():
     tela_veiculos_cadastrar.show()
@@ -181,14 +104,18 @@ def tela_dados_viagens():
 app=QtWidgets.QApplication([])
 
 #Telas Relacionadas aos Veículos
-tela_veiculos_cadastrar = uic.loadUi("cadastrar_veiculos.ui")
 tela_veiculos_listar = uic.loadUi("listarveiculos.ui")
-tela_veiculos_menu_veiculos = uic.loadUi("menu_veiculos.ui")
-tela_veiculos_dados = uic.loadUi("dados_veiculos.ui")
+
+tela_menu_veiculos = uic.loadUi("menu_veiculos.ui")
+tela_menu_veiculos.bot_cadastrar_veiculos.clicked.connect(tela_cadastrar_veiculos)
+tela_menu_veiculos.bot_cadastrar_viagens.clicked.connect(tela_dados_viagens)
+tela_menu_veiculos.bot_menu.clicked.connect(tela_menu_principal)
+
+tela_veiculos_cadastrar = uic.loadUi("cadastrar_veiculos.ui")
 tela_veiculos_cadastrar.pushButton.clicked.connect(cadastrar_veiculos)
 tela_veiculos_cadastrar.pushButton_2.clicked.connect(listar_dados_veiculos)
-tela_veiculos_menu_veiculos.pushButton.clicked.connect(tela_cadastrar_veiculos)
-tela_veiculos_menu_veiculos.pushButton_2.clicked.connect(tela_dados_viagens)
+
+tela_veiculos_dados = uic.loadUi("dados_veiculos.ui")
 tela_veiculos_dados.pushButton.clicked.connect(inserir_dados_viagens)
 tela_veiculos_dados.pushButton_2.clicked.connect(listar_dados_viagens)
 
@@ -196,21 +123,7 @@ tela_veiculos_dados.pushButton_2.clicked.connect(listar_dados_viagens)
 tela_login = uic.loadUi("login.ui")
 tela_menu = uic.loadUi("Menu.ui")
 tela_login.pushButton.clicked.connect(login)
-tela_menu.pushButton.clicked.connect(salvar_dados_funcionarios)
-tela_menu.pushButton_2.clicked.connect(menu_veiculos)
-tela_menu.pushButton_3.clicked.connect(salvar_dados_cidades)
-
-#Telas relacionadas aos Funcionários
-tela_funcionarios=uic.loadUi("cadastrar_funcionario.ui")
-tela_funcionarios_2 = uic.loadUi("listarfuncionario.ui")
-tela_funcionarios.pushButton.clicked.connect(salvar_dados_funcionarios)
-tela_funcionarios.pushButton_2.clicked.connect(listar_dados_funcionarios)
-
-#Telas Relacionadas as Cidades
-tela_cidades=uic.loadUi("cadastrar_cidades.ui")
-tela_cidades_2 = uic.loadUi("listarcidades.ui")
-tela_cidades.pushButton.clicked.connect(salvar_dados_cidades)
-tela_cidades.pushButton_2.clicked.connect(listar_dados_cidades)
+tela_menu.menu_veiculos.clicked.connect(menu_veiculos)
 
 tela_login.show()
 app.exec()
